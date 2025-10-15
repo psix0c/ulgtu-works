f702ee37161ec54acce366fff407f4fded54d81a
psix0c <lukasin658@gmail.com>
2025-10-15 21:23:51 +0400

    /\

--- Исхаков 5 лаба/main.c
+++ Исхаков 5 лаба/main.c
@@ -1,62 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main() {
     system("chcp 65001 > nul");
-
     int n;
-    printf("Введите количество элементов массива: ");
+    printf("Введите количество чисел: ");
     scanf("%d", &n);
 
-    if (n <= 1) {
-        printf("Недостаточно элементов для поиска.\n");
+    if (n < 2) {
+        printf("Мало чисел.\n");
         return 0;
     }
 
-    int arr[n];
-
-    printf("Введите элементы массива через пробел: ");
+    int a[n];
+    printf("Введите числа: ");
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &a[i]);
     }
 
-    int min_val = arr[0];
-    int max_val = arr[0];
-    int min_index = 0;
-    int max_index = 0;
+    int min = a[0], max = a[0];
+    int i_min = 0, i_max = 0;
 
     for (int i = 1; i < n; i++) {
-        if (arr[i] < min_val) {
-            min_val = arr[i];
-            min_index = i;
+        if (a[i] < min) {
+            min = a[i];
+            i_min = i;
         }
-        if (arr[i] > max_val) {
-            max_val = arr[i];
-            max_index = i;
-        }
-    }
-
-    if (min_index > max_index) {
-        int temp = min_index;
-        min_index = max_index;
-        max_index = temp;
-    }
-
-    printf("Четные элементы между минимальным (%d) и максимальным (%d):\n", min_val, max_val);
-
-    int found = 0;
-    for (int i = min_index + 1; i < max_index; i++) {
-        if (arr[i] % 2 == 0) {
-            printf("%d ", arr[i]);
-            found = 1;
+        if (a[i] > max) {
+            max = a[i];
+            i_max = i;
         }
     }
 
-    if (!found) {
-        printf("Таких элементов нет.\n");
+    if (i_min > i_max) {
+        int t = i_min;
+        i_min = i_max;
+        i_max = t;
     }
 
-    printf("\n");
+    printf("Четные числа между %d и %d:\n", min, max);
+    int ok = 0;
+    for (int i = i_min + 1; i < i_max; i++) {
+        if (a[i] % 2 == 0) {
+            printf("%d ", a[i]);
+            ok = 1;
+        }
+    }
+
+    if (!ok)
+        printf("Нет четных чисел.\n");
 
     return 0;
 }
